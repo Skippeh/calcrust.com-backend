c5a2692d8d19792f7974cb0bce244df0be75c0d0
Jonathan Lindahl <jonathanjl99@gmail.com>
2016-11-21 21:50:26 +0100

    Added ApiUrl setting.

--- DiscordBot/Settings.cs
+++ DiscordBot/Settings.cs
@@ -6,5 +6,8 @@ namespace DiscordBot
     {
         [ValueArgument(typeof (string), 't', "token", Optional = false, Description = "The discord bot token used for authentication.")]
         public string Token { get; private set; } = "";
+
+        [ValueArgument(typeof (string), 'a', "apiurl", Optional = true, Description = "The rust api to fetch data from.", FullDescription = "Defaults to 'https://api.calcrust.com/'. Note that the url needs to end with a slash.")]
+        public string ApiUrl { get; private set; } = "https://api.calcrust.com/";
     }
 }
