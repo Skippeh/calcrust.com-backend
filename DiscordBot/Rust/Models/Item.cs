cd7133941debef9a85e739718158798d77c6522e
Jonathan Lindahl <jonathanjl99@gmail.com>
2016-11-22 14:04:27 +0100

    Item implements IEquatable<Item>.

--- DiscordBot/Rust/Models/Item.cs
+++ DiscordBot/Rust/Models/Item.cs
@@ -1,9 +1,10 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace DiscordBot.Rust.Models
 {
-    public sealed class Item
+    public sealed class Item : IEquatable<Item>
     {
         [JsonProperty("shortname")] public string Id;
         public string Name;
@@ -12,5 +13,10 @@ namespace DiscordBot.Rust.Models
         public string Category;
         public JObject Meta; // Todo: Parse meta
         public string[] Descriptions;
+        
+        public bool Equals(Item other)
+        {
+            return other != null && Id == other.Id;
+        }
     }
 }
