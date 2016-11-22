597dd7c065b709022bee80c60aff2d792798543b
Jonathan Lindahl <jonathanjl99@gmail.com>
2016-11-22 13:57:15 +0100

    Fixed not setting default ValueArgument value properly.

--- DiscordBot/Settings.cs
+++ DiscordBot/Settings.cs
@@ -4,10 +4,10 @@ namespace DiscordBot
 {
     public class Settings
     {
-        [ValueArgument(typeof (string), 't', "token", Optional = false, Description = "The discord bot token used for authentication.")]
-        public string Token { get; private set; } = "";
+        [ValueArgument(typeof (string), 't', "token", Optional = false, DefaultValue = "", Description = "The discord bot token used for authentication.")]
+        public string Token { get; private set; }
 
-        [ValueArgument(typeof (string), 'a', "apiurl", Optional = true, Description = "The rust api to fetch data from.", FullDescription = "Defaults to 'https://api.calcrust.com/'. Note that the url needs to end with a slash.")]
-        public string ApiUrl { get; private set; } = "https://api.calcrust.com/";
+        [ValueArgument(typeof (string), 'a', "apiurl", Optional = true, DefaultValue = "https://api.calcrust.com/", Description = "The rust api to fetch data from.", FullDescription = "Defaults to 'https://api.calcrust.com/'. Note that the url needs to end with a slash.")]
+        public string ApiUrl { get; private set; }
     }
 }
