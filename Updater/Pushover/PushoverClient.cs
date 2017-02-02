f5550cded367962b24121482917fa24c0bc5920d
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-02-02 21:00:53 +0100

    Send proper pushover request and read response success.

--- Updater/Pushover/PushoverClient.cs
+++ Updater/Pushover/PushoverClient.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Net;
+using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace Updater.Pushover
 {
@@ -14,22 +18,53 @@ namespace Updater.Pushover
         {
             ApiKey = apiKey;
             UserKey = userKey;
-            webClient.Headers["token"] = ApiKey;
-            webClient.Headers["user"] = UserKey;
+
+            webClient = new WebClient
+            {
+                Headers =
+                {
+                    ["token"] = ApiKey,
+                    ["user"] = UserKey
+                }
+            };
         }
 
         public async Task<bool> PushMessage(PushoverMessage message)
         {
-            webClient.Headers["message"] = message.Message;
-            webClient.Headers["title"] = message.Title;
-            webClient.Headers["url"] = message.Url;
-            webClient.Headers["url_title"] = message.UrlTitle;
-            webClient.Headers["priority"] = ((int) message.Priority).ToString();
-            webClient.Headers["sound"] = message.Sound.ToString();
+            byte[] byteResponse;
 
-            byte[] byteResponse = await webClient.UploadDataTaskAsync("https://api.pushover.net/1/messages.json", new byte[] {});
+            try
+            {
+                byteResponse = await webClient.UploadValuesTaskAsync("https://api.pushover.net/1/messages.json", new NameValueCollection
+                {
+                    { "token", ApiKey },
+                    { "user", UserKey },
+                    { "message", message.Text },
+                    { "title", message.Title },
+                    { "url", message.Url },
+                    { "url_title", message.UrlTitle },
+                    { "priority", ((int)message.Priority).ToString() },
+                    { "sound", message.Sound.ToString().ToLower() }
+                });
+            }
+            catch (WebException ex)
+            {
+                var stream = ex.Response.GetResponseStream();
 
-            return true; // Assume success for now. todo: read response
+                if (stream == null)
+                    return false;
+
+                if (stream.Length <= 0)
+                    return false;
+
+                byteResponse = new byte[stream.Length];
+                await stream.ReadAsync(byteResponse, 0, (int) stream.Length);
+            }
+
+            string response = Encoding.UTF8.GetString(byteResponse);
+            JObject jResponse = JObject.Parse(response);
+
+            return jResponse["status"].Value<int>() == 1;
         }
     }
 }
