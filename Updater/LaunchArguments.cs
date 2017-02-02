02828a59bec3dd4bce8ed9d63bc674d3e6887740
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-02-02 21:32:42 +0100

    Renamed Config to LaunchArguments and added some more parameters.

---
+++ Updater/LaunchArguments.cs
@@ -0,0 +1,22 @@
+﻿using CommandLineParser.Arguments;
+
+namespace Updater
+{
+    public class LaunchArguments
+    {
+        [ValueArgument(typeof(bool), 'h', "help")]
+        public bool ShowHelp;
+
+        [ValueArgument(typeof(string), 'a', "pushoverApiKey", Description = "Api key for Pushover. If specified along with user key, notifications will be posted on certain events.")]
+        public string PushoverApiKey;
+
+        [ValueArgument(typeof(string), 'u', "pushoverUserKey")]
+        public string PushoverUserKey;
+
+        [ValueArgument(typeof(string), 'b', "branch", DefaultValue = "public", Description = "The game branch to use, for example staging or prerelease.")]
+        public string Branch;
+
+        [ValueArgument(typeof(uint), 'i', "interval", Description = "Seconds between checking for updates.", DefaultValue = 120u)]
+        public uint CheckInterval;
+    }
+}
