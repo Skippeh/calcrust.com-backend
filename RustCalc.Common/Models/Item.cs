0d71a8e68e920cf382e820a0c5109b8899f249b3
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-03-20 11:53:09 +0100

    Removed ItemData stuff (flawed and unnecessary)

--- RustCalc.Common/Models/Item.cs
+++ RustCalc.Common/Models/Item.cs
@@ -11,13 +11,7 @@ namespace RustCalc.Common.Models
         public ItemCategory Category { get; set; }
         public int StackSize { get; set; }
         public int ItemId { get; set; }
-        public SerializableDictionary<string, ItemData> Data { get; private set; }
-
-        public Item()
-        {
-            Data = new SerializableDictionary<string, ItemData>();
-        }
-
+        
         void IBinarySerializable.Serialize(BinaryWriter writer)
         {
             writer.WriteNullable(Name);
@@ -25,9 +19,6 @@ namespace RustCalc.Common.Models
             writer.Write((int)Category);
             writer.Write(StackSize);
             writer.Write(ItemId);
-
-            Data.OnSerializeItem = (itemData, writer2) => writer2.Write(itemData.GetType().GetTypeName());
-            writer.Write(Data);
         }
 
         void IBinarySerializable.Deserialize(BinaryReader reader)
@@ -37,9 +28,6 @@ namespace RustCalc.Common.Models
             Category = (ItemCategory) reader.ReadInt32();
             StackSize = reader.ReadInt32();
             ItemId = reader.ReadInt32();
-
-            Data.OnDeserializeItem = (type, reader2) => (ItemData)Activator.CreateInstance(Type.GetType(reader2.ReadString()), true);
-            Data = reader.Deserialize<SerializableDictionary<string, ItemData>>();
         }
 
         public override string ToString()
