6c074d60386d766bfce3d2fcc2af8a8c3787e560
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-03-27 14:19:19 +0200

    Added api module for skins

---
+++ RustCalc.Api/V1/Modules/SkinsModuleV1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Nancy;
+using RustCalc.Common.Models;
+
+namespace RustCalc.Api.V1.Modules
+{
+    public class SkinsModuleV1 : RustCalcModuleV1
+    {
+        public SkinsModuleV1() : base("/skins")
+        {
+            Get["/"] = WrapMethod(_ => GetSkins());
+            Get["/{shortname}"] = WrapMethod((dynamic _) => GetItemSkins((string) _.shortname));
+        }
+
+        private Dictionary<string, List<ItemSkin>> GetSkins()
+        {
+            return SkinsManager.Skins.ToDictionary(kv => kv.Key.Shortname, kv => kv.Value);
+        }
+
+        private List<ItemSkin> GetItemSkins(string shortname)
+        {
+            string lowerShortname = shortname.ToLower();
+            var skinList = SkinsManager.Skins.FirstOrDefault(kv => kv.Key.Shortname.ToLower() == lowerShortname).Value;
+
+            if (skinList == null)
+                throw new ApiResponseException(HttpStatusCode.NotFound, $"No item found with shortname '{shortname}'.");
+
+            return skinList;
+        }
+    }
+}
