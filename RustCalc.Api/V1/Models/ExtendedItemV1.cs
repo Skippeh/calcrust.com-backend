821722c78d93645f826b44f78b0ee031d79079c3
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-03-27 21:45:44 +0200

    Added api module for items

---
+++ RustCalc.Api/V1/Models/ExtendedItemV1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RustCalc.Common.Models;
+
+namespace RustCalc.Api.V1.Models
+{
+    public class ExtendedItemV1
+    {
+        public string Name => item.Name;
+        public string Shortname => item.Shortname;
+        public ItemCategory Category => item.Category;
+        public int StackSize => item.StackSize;
+        public int ItemId => item.ItemId;
+
+        public readonly List<string> Tags = new List<string>(); 
+
+        private readonly ExportData data;
+        private readonly Item item;
+
+        public ExtendedItemV1(Item item, ExportData data)
+        {
+            this.item = item;
+            this.data = data;
+
+            if (data.Recipes.Any(recipe => recipe.Key == item))
+            {
+                Tags.Add("recipe");
+            }
+
+            if (data.Destructibles.Any(kv => kv.Key == item.Shortname))
+            {
+                Tags.Add("destructible");
+            }
+
+            if (data.Recycler.Any(kv => kv.Key == item))
+            {
+                Tags.Add("recyclable");
+            }
+        }
+    }
+}
---
+++ RustCalc.Api/V1/Modules/ItemsModuleV1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Nancy;
+using RustCalc.Api.V1.Models;
+using RustCalc.Common.Models;
+
+namespace RustCalc.Api.V1.Modules
+{
+    public class ItemsModuleV1 : RustCalcModuleV1
+    {
+        public ItemsModuleV1() : base("/items")
+        {
+            Get["/"] = WrapMethod(_ => GetItems());
+            Get["/{shortname}"] = WrapMethod((dynamic _) => GetItem(_.shortname));
+        }
+
+        private List<Item> GetItems()
+        {
+            return Data.Items.Values.ToList();
+        }
+
+        public ExtendedItemV1 GetItem(string shortname)
+        {
+            var lowerShortname = shortname.ToLower();
+            var item = Data.Items.FirstOrDefault(kv => kv.Value.Shortname.ToLower() == lowerShortname).Value;
+
+            if (item == null)
+                throw new ApiResponseException(HttpStatusCode.NotFound, $"No item found with shortname '{shortname}'.");
+
+            var extendedItem = new ExtendedItemV1(item, Data);
+            return extendedItem;
+        }
+    }
+}
