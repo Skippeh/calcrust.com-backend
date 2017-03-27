7716461d9d5f11d46dd8e285dc927895fce37203
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-03-27 14:34:44 +0200

    Modified how Data is set based on "branch" header value. Verified and assigned before request.

--- RustCalc.Api/RustCalcModule.cs
+++ RustCalc.Api/RustCalcModule.cs
@@ -7,32 +7,38 @@ namespace RustCalc.Api
 {
     public abstract class RustCalcModule : NancyModule
     {
-        private ExportData data;
-        protected ExportData Data
-        {
-            get
-            {
-                if (data != null)
-                    return data;
-
-                string branch = Request.Headers["branch"].FirstOrDefault();
-
-                if (branch == null)
-                    return data = Program.Data[GameBranch.Public];
-
-                GameBranch branchValue;
-                if (Enum.TryParse(branch, out branchValue))
-                {
-                    return data = Program.Data[branchValue];
-                }
-
-                return data = null;
-            }
-        }
+        protected ExportData Data { get; private set; }
 
         public RustCalcModule(string modulePath) : base(modulePath)
         {
-            
+            Before.AddItemToEndOfPipeline(context =>
+            {
+                string branch = context.Request.Headers["branch"].FirstOrDefault() ?? "public";
+
+                GameBranch gameBranch;
+                if (!Enum.TryParse(branch, true, out gameBranch))
+                {
+                    return new Response
+                    {
+                        StatusCode = HttpStatusCode.BadRequest,
+                        ContentType = "application/json",
+                        ReasonPhrase = "Branch Not Found"
+                    };
+                }
+
+                if (!Program.Data.ContainsKey(gameBranch))
+                {
+                    return new Response
+                    {
+                        StatusCode = HttpStatusCode.NotFound,
+                        ContentType = "application/json",
+                        ReasonPhrase = "Branch Not Loaded"
+                    };
+                }
+
+                Data = Program.Data[gameBranch];
+                return null;
+            });
         }
     }
 }
